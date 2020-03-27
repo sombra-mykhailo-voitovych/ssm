3d3671f27d35caa41aa7753af003cdb3e12377cb
Mykhailo Voitovych <mykhailo@sombrainc.com>
2020-03-27 16:24:50 +0200

    BREAKING CHANGE: remove persister for single state machine instance

--- src/main/java/com/sombrainc/ssm/service/LeadService.java
+++ src/main/java/com/sombrainc/ssm/service/LeadService.java
@@ -1,9 +1,7 @@
 package com.sombrainc.ssm.service;
 
 import com.sombrainc.ssm.events.LeadEvents;
-import com.sombrainc.ssm.persister.LeadStateMachinePersister;
 import com.sombrainc.ssm.states.LeadStates;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.stereotype.Service;
@@ -16,26 +14,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class LeadService {
 
-    private final LeadStateMachinePersister stateMachinePersister;
     private final StateMachine<LeadStates, LeadEvents> stateMachine;
 
     @Autowired
     public LeadService(
-            LeadStateMachinePersister stateMachinePersister,
             StateMachine<LeadStates, LeadEvents> stateMachine) {
-        this.stateMachinePersister = stateMachinePersister;
         this.stateMachine = stateMachine;
-
-        final String leadId = RandomStringUtils.randomAlphabetic(12);
-        stateMachine.getExtendedState().getVariables().put("lead_id", leadId);
-        stateMachinePersister.persist(stateMachine, leadId);
     }
 
     public void updateLeadStateByEvent(LeadEvents event){
-        final String leadId = (String) stateMachine.getExtendedState().getVariables().get("lead_id");
         final boolean eventResult = stateMachine.sendEvent(event);
-        if(eventResult){
-            stateMachinePersister.persist(stateMachine, leadId);
+        if (!eventResult){
+            throw new RuntimeException(String.format("event %s is not accepted", event));
         }
     }
 
--- src/test/java/com/sombrainc/ssm/LeadServiceTest.java
+++ src/test/java/com/sombrainc/ssm/LeadServiceTest.java
@@ -1,7 +1,6 @@
 package com.sombrainc.ssm;
 
 import com.sombrainc.ssm.events.LeadEvents;
-import com.sombrainc.ssm.persister.LeadStateMachinePersister;
 import com.sombrainc.ssm.service.LeadService;
 import com.sombrainc.ssm.states.LeadStates;
 import org.junit.Test;
@@ -23,29 +22,22 @@ public class LeadServiceTest {
     private StateMachine<LeadStates, LeadEvents> stateMachine;
 
     @Autowired
-    private LeadStateMachinePersister leadStateMachinePersister;
-
-    @Autowired
     private LeadService leadService;
 
     @Test
     public void whenAssignSell_thenReturnSoldFromPersister(){
-        final String leadId = (String) stateMachine.getExtendedState().getVariables().get("lead_id");
+
+        assertEquals(stateMachine.getState().getId(), LeadStates.NEW);
 
         leadService.updateLeadStateByEvent(LeadEvents.ASSIGN);
         leadService.updateLeadStateByEvent(LeadEvents.SELL);
 
-        final StateMachine<LeadStates, LeadEvents> stateMachineStored =
-                leadStateMachinePersister.restore(stateMachine, leadId);
-        assertEquals(stateMachineStored.getState().getId(), LeadStates.SOLD);
+        assertEquals(stateMachine.getState().getId(), LeadStates.SOLD);
     }
 
     @Test
     public void whenNoActionPerformed_thenReturnNewFromPersister(){
-        final String leadId = (String) stateMachine.getExtendedState().getVariables().get("lead_id");
-        final StateMachine<LeadStates, LeadEvents> stateMachineStored =
-                leadStateMachinePersister.restore(stateMachine, leadId);
-        assertEquals(stateMachineStored.getState().getId(), LeadStates.NEW);
+        assertEquals(stateMachine.getState().getId(), LeadStates.NEW);
     }
 
 }
